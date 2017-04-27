3305fd2126257338c28bd130266e4f18374068b3
kswork <ksekimoto@hpe.com>
2017-04-27 15:35:54 +0900

    revert to original

--- ceilosca/ceilometer/monasca_client.py
+++ ceilosca/ceilometer/monasca_client.py
@@ -107,8 +107,7 @@ class Client(object):
         kwargs = {
             'username': username,
             'password': password,
-            #'auth_url': auth_url.replace("v2.0", "v3"),
-            'auth_url':  auth_url + "/v3",
+            'auth_url': auth_url.replace("v2.0", "v3"),
             'project_id': project_id,
             'project_name': project_name,
             'region_name': conf.region_name,
